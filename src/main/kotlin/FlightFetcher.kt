13a77fba2ce79240b68d35197dc9170cd06d14ac
WerdnaLes <werdnales@gmail.com>
2022-09-28 15:45:55 +0300

    Added Flow

--- src/main/kotlin/FlightFetcher.kt
+++ src/main/kotlin/FlightFetcher.kt
@@ -1,33 +1,15 @@
-import io.ktor.client.HttpClient
+
+import io.ktor.client.*
 import io.ktor.client.call.*
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.request.get
-import kotlinx.coroutines.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 
 private const val BASE_URL = "http://kotlin-book.bignerdranch.com/2e"
 private const val FLIGHT_ENDPOINT = "$BASE_URL/flight"
 private const val LOYALTY_ENDPOINT = "$BASE_URL/loyalty"
-
-fun main() {
-    runBlocking {
-        println("Started")
-        launch {
-            var flight = fetchFlight("Satyricon")
-
-            while (flight.status == "Canceled") {
-                println("The flight ${flight.flightNumber} is canceled, searching for another flight...")
-                flight = fetchFlight("Satyr")
-            }
-            println(flight)
-        }
-        println("Finished")
-    }
-
-//    val seconds = (listInMillis / 1000) % 60
-//    val minutes = ((listInMillis / 1000) % 3600) / 60
-//    val hours = (listInMillis / 1000) / 3600
-//    println("List completed in ${"%d:%02d:%02d".format(Locale.US, hours, minutes, seconds)}")
-}
 
 suspend fun fetchFlight(passengerName: String): FlightStatus = coroutineScope {
 
---
+++ src/main/kotlin/FlightWatcher.kt
@@ -0,0 +1,56 @@
+import BoardingState.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        println("Getting the latest flight info...")
+        val flights = fetchFlights()
+        val flightDescriptions = flights.joinToString {
+            "${it.passengerName} (${it.flightNumber})"
+        }
+        println("Found flights for $flightDescriptions")
+
+
+        flights.forEach {
+            watchFlight(it)
+        }
+    }
+}
+
+suspend fun watchFlight(initialFlight: FlightStatus) {
+    val passengerName = initialFlight.passengerName
+    val currentFlight: Flow<FlightStatus> = flow {
+        var flight = initialFlight
+        while (flight.departureTimeInMinutes >= 0 &&
+            !flight.isFlightCanceled
+        ) {
+            emit(flight)
+            delay(1000)
+            flight = flight.copy(
+                departureTimeInMinutes =
+                flight.departureTimeInMinutes - 1
+            )
+        }
+    }
+    currentFlight.collect {
+        val status = when (it.boardingStatus) {
+            FlightCanceled -> "Your flight was canceled"
+            BoardingNotStarted -> "Boarding will start soon"
+            WaitingToBoard -> "Other passengers are boarding"
+            Boarding -> "You can now board the plane"
+            BoardingEnded -> "The boarding doors have closed"
+        } + " (Flight departs in ${it.departureTimeInMinutes} minutes)"
+        println("$passengerName: $status")
+    }
+    println("Finished tracking $passengerName's flight")
+}
+
+suspend fun fetchFlights(
+    passengerNames: List<String> = listOf(
+        "Satyricon",
+        "Polarcubis"
+    )
+) = passengerNames.map { fetchFlight(it) }
