47937926049628f3c572d54569ed4b4efebbbd06
WerdnaLes <werdnales@gmail.com>
2022-09-28 11:44:57 +0300

    Coroutines passed

--- src/main/kotlin/FlightFetcher.kt
+++ src/main/kotlin/FlightFetcher.kt
@@ -2,11 +2,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.request.get
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import java.net.URL
+import kotlinx.coroutines.*
 
 private const val BASE_URL = "http://kotlin-book.bignerdranch.com/2e"
 private const val FLIGHT_ENDPOINT = "$BASE_URL/flight"
@@ -16,21 +12,43 @@ fun main() {
     runBlocking {
         println("Started")
         launch {
-            val flight = fetchFlight()
+            var flight = fetchFlight("Satyricon")
+
+            while (flight.status == "Canceled") {
+                println("The flight ${flight.flightNumber} is canceled, searching for another flight...")
+                flight = fetchFlight("Satyr")
+            }
             println(flight)
         }
         println("Finished")
     }
 
-    //    val seconds = (listInMillis / 1000) % 60
+//    val seconds = (listInMillis / 1000) % 60
 //    val minutes = ((listInMillis / 1000) % 3600) / 60
 //    val hours = (listInMillis / 1000) / 3600
 //    println("List completed in ${"%d:%02d:%02d".format(Locale.US, hours, minutes, seconds)}")
 }
 
-suspend fun fetchFlight(): String {
+suspend fun fetchFlight(passengerName: String): FlightStatus = coroutineScope {
+
     val client = HttpClient(CIO)
-    val flightResponse = client.get(FLIGHT_ENDPOINT).body<String>()
-    val loyaltyResponse = client.get(LOYALTY_ENDPOINT).body<String>()
-    return "$flightResponse\n$loyaltyResponse"
+    val flightResponse = async {
+        println("Started fetching flight info")
+        client.get(FLIGHT_ENDPOINT).body<String>().also {
+            println("Finished fetching flight info")
+        }
+    }
+    val loyaltyResponse = async {
+        println("Started fetching loyalty info")
+        client.get(LOYALTY_ENDPOINT).body<String>().also {
+            println("Finished fetching loyalty info")
+        }
+    }
+    delay(500)
+    println("Combining flight data")
+    FlightStatus.parse(
+        flightResponse = flightResponse.await(),
+        loyaltyResponse = loyaltyResponse.await(),
+        passengerName = passengerName
+    )
 }
